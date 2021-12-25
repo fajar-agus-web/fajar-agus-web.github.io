59758816c4ef6b3abf9c9ad8b835ca355cc8cb8c
fajar-agus-web <96648780+fajar-agus-web@users.noreply.github.com>
2021-12-25 16:22:59 +0700

    Add files via upload

---
+++ js/global.js
@@ -0,0 +1,24 @@
+var btn = document.getElementById('btn-action');
+var nama = document.getElementById('input-nama');
+var email = document.getElementById('input-email');
+var msg = document.getElementById('msg');
+var validation = false;
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    validation = false;
+    [nama, email, msg].forEach(function(el) {
+        if (el.value === null || el.value === '') {
+            validation = true;
+        }
+    });
+
+    if (validation) {
+        alert('Tidak boleh ada kolom yang kosong');
+    } else {
+        nama.value = '';
+        email.value = '';
+        msg.value = '';
+        alert('Pesan berhasil dikirim');
+    }
+
+});
