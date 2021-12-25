8f52eb66786a0ef80376c26913da980228481385
fajar-agus-web <96648780+fajar-agus-web@users.noreply.github.com>
2021-12-25 22:17:53 +0700

    Add files via upload

--- js/global.js
+++ js/global.js
@@ -22,3 +22,12 @@ btn.addEventListener('click', function(e) {
     }
 
 });
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
